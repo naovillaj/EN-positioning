c0c60b3700f445d7d6f90b81f523d5ab58249f27
Naomi Villanueva <naovillaj@gmail.com>
2017-04-18 16:10:12 -0500

    creando divs

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var div1 = document.createElement("div");
+var div2 = document.createElement("div");
+var div3 = document.createElement("div");
+var div4 = document.createElement("div");
+var div5 = document.createElement("div");
+var div6 = document.createElement("div");
